c136e1768153a3428652f58e833141d424fd8a07
lyulingxue <lyulingxue@hotmail.com>
2020-10-25 21:14:48 -0400

    spoilerplate set up

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://Lingxue:'+ encodeURIComponent('Lingxue') + '@cluster0-w3qyo.mongodb.net/cis557?retryWrites=true&w=majority',
+  secretOrKey: "secret"
+};
