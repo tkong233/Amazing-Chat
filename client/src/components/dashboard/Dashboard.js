89d88c35332a92d8520c9ff753e0d23042d46ef2
tkong233 <tkong@wesleyan.edu>
2020-11-19 23:22:26 -0500

    stage commits before pull from main

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,27 +1,17 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { logoutUser } from "../../actions/authActions";
-import UserInfo from './UserInfo';
+
+import UserInfoCard from './UserInfoCard';
 
 
 const Dashboard = (props) => {
-
-  const onLogoutClick = e => {
-    e.preventDefault();
-    props.logoutUser();
-  };
-
   return (
     <div>
       {/* <Alert severity="success">This is a success alert — check it out!</Alert> */}
-      <UserInfo/>
+      <UserInfoCard/>
       {/* Logout Button */}
-      <button
-        onClick={ onLogoutClick }
-      >
-        Logout
-      </button>
+      
     </div>
   );
 }
@@ -32,11 +22,4 @@ Dashboard.propTypes = {
   auth: PropTypes.object.isRequired
 };
 
-const mapStateToProps = state => ({
-  auth: state.auth,
-});
-
-export default connect(
-  mapStateToProps,
-  { logoutUser }
-)(Dashboard);
+export default Dashboard;
--- client/src/components/dashboard/UserInfoCard.js
+++ client/src/components/dashboard/UserInfoCard.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { connect } from "react-redux";
+import { logoutUser } from "../../actions/authActions";
 import Avatar from '@material-ui/core/Avatar';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   avatar: {
     height: 100,
     width: 100,
   }
 }));
 
-const UserInfo = (props) => {
+
+const UserInfoCard = (props) => {
   const { name, profile_picture, email } = props.user;
   const classes = useStyles();
+  const onLogoutClick = e => {
+    e.preventDefault();
+    props.logoutUser();
+  };
 
   return (
     <div>
@@ -30,15 +36,29 @@ const UserInfo = (props) => {
         <div>
           { email }
         </div>
+        <button
+          onClick={ onLogoutClick }
+          style={{
+                    width: "140px",
+                    borderRadius: "3px",
+                    letterSpacing: "1.5px"
+                  }}
+          className="btn waves-effect waves-light hoverable blue-grey darken-1"
+        >
+        Logout
+      </button>
       </div>
     </div>
   );
 }
 
 const mapStateToProps = state => ({
+  auth: state.auth,
   user: state.auth.user,
 });
 
+
 export default connect(
-  mapStateToProps
-)(UserInfo);
+  mapStateToProps,
+  { logoutUser }
+)(UserInfoCard);
