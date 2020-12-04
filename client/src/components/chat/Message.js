e6712706fbf4257ff3d368352868afda58ce16e7
Fangyu Yan <yfangyu@seas.upenn.edu>
2020-12-04 03:02:31 -0500

    add video and audio

--- client/src/components/chat/Message.js
+++ client/src/components/chat/Message.js
@@ -1,44 +1,65 @@
 import React from 'react';
 import './Message.css';
-
 import { MessageBox } from 'react-chat-elements';
 import 'react-chat-elements/dist/main.css';
 
 const Message = (props) => {
   const { text, sender, user, type } = props;
+  console.log(props)
   const position = (sender === user) ? 'right' : 'left';
-  
-  console.log(props.text);
-  return (
-    props.text.substring(0, 8) === "uploads/"?
-    props.text.substring(props.text.length - 3, props.text.length) === 'mp4'?
-    <MessageBox
-      className="message-bubble"
-      position={position}
-      type={"video"}
-      data={{
-        uri: `${props.text}`
-        // uri: `uploads/sample-mp4-file.mp4`
-        // uri: 'https://www.w3schools.com/html/mov_bbb.mp4'
-      }}
-    />
-    :
-    <MessageBox
-      className="message-bubble"
-      position={position}
-      type={"photo"}
-      data={{
-        uri: `${props.text}`
-      }}
-    />
-  :
+
+  if (props.text.substring(0, 8) === "uploads/"){
+    const file_ext = props.text.substring(props.text.length - 3, props.text.length);
+      if ( file_ext === 'mp4' || file_ext === 'webm' || file_ext === 'avi' || file_ext === 'mov'){
+        return (
+          <div>
+            <video
+            style={{ maxWidth: '500px' }}
+            src={`${props.text}`} alt="video"
+            type="video/mp4" controls
+          />
+          </div>
+        );
+      }else if (file_ext === 'mp3' || file_ext === 'wav' || file_ext === 'ogg'){
+        return(
+          <div>
+            <audio
+            src={`${props.text}`} alt="audio"
+            type="audio/ogg" controls
+          />
+          </div> 
+        );
+      }else if(file_ext === 'png' || file_ext === 'jpg'){
+        return(
+          <MessageBox
+          className="message-bubble"
+          position={position}
+          type={"photo"}
+          data={{
+            uri: `${props.text}`,
+          }}
+        />
+        );
+      }else{
+        return(
+          <MessageBox
+            className="message-bubble"
+            position={position}
+            type={"text"}
+            text={props.text}
+          />
+        )
+      }
+  }else{
+    return(
       <MessageBox
         className="message-bubble"
         position={position}
         type={type}
         text={text}
       />
-  );
+    );
+  }
 }
 
 export default Message;
