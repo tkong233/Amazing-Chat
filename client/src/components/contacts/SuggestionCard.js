ff78de6fa4c4bbcc4028bee8c206360315cf5690
lyulingxue <lyulingxue@hotmail.com>
2020-12-18 14:39:44 -0500

    fixed style

--- client/src/components/contacts/SuggestionCard.js
+++ client/src/components/contacts/SuggestionCard.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from "react";
 import { connect } from "react-redux";
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import { addContact, getSuggestion } from '../../actions/contactActions';
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import { addContact, getSuggestion } from "../../actions/contactActions";
 
 const addContactFromSuggestion = (props, email1, email2) => {
   props.addContact(email1, email2);
   props.getSuggestion(email1);
-}
+};
 
 const SuggestionCard = (props) => {
   const { name, profilePicture, userEmail, suggestionEmail } = props;
@@ -19,28 +19,25 @@ const SuggestionCard = (props) => {
   return (
     <ListItem>
       <ListItemAvatar>
-        <Avatar alt={name} src={profilePicture}/>
+        <Avatar alt={name} src={profilePicture} />
       </ListItemAvatar>
-      <ListItemText
-      primary={name}
-    />
-    <ListItemSecondaryAction>
-      <Button
-        variant="outlined"
-        onClick= { () => addContactFromSuggestion(props, userEmail, suggestionEmail) }
-      >
-        Add
-      </Button>
-    </ListItemSecondaryAction>
+      <ListItemText primary={name} />
+      <ListItemSecondaryAction>
+        <Button
+          variant="outlined"
+          onClick={() =>
+            addContactFromSuggestion(props, userEmail, suggestionEmail)
+          }
+        >
+          Add
+        </Button>
+      </ListItemSecondaryAction>
     </ListItem>
-  )
-}
+  );
+};
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({});
 
-});
-
-export default connect(
-  mapStateToProps,
-  { addContact, getSuggestion }
-)(SuggestionCard);
+export default connect(mapStateToProps, { addContact, getSuggestion })(
+  SuggestionCard
+);
